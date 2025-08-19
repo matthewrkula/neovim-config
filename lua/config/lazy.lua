965a9bcb46707dd75362e0d3f10d674626ec5ffb
Matt Kula <matt.kula@perplexity.ai>
2025-08-19 11:14:04 -0600

    Disable update notifs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,6 +33,6 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- automatically check for plugin updates, don't notify
+  checker = { enabled = true, notify = false },
 })
